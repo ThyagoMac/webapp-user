8f4f14e8cd586a34954249052f9a149305b654bd
ThyagoMac <thyago.cormick@gmail.com>
2020-06-30 14:59:13 -0300

    Test

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,27 +1,47 @@
 <template>
     <div>
         <h1>Lista de Usuários</h1>
-        <p>component Home.vue</p>
+        <hr>
+        <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                prop="date"
+                label="Date"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="Name"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="Address">
+            </el-table-column>
+        </el-table>
+        <hr>
+        <br>
         <hr><br>
 
         <el-row :gutter="20">
-            <el-col :span="4"><div class="grid-content bg-purple"></div>Data de Cadastro</el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div>Nome</el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div>Ano de Nascimento</el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div>Email</el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div>Login</el-col>
+            <el-col :span="4"><div class=""></div>Data de Cadastro</el-col>
+            <el-col :span="4"><div class=""></div>Nome</el-col>
+            <el-col :span="4"><div class=""></div>Ano de Nascimento</el-col>
+            <el-col :span="4"><div class=""></div>Email</el-col>
+            <el-col :span="4"><div class=""></div>Login</el-col>
         </el-row>
-        <ul class="lista-fotos">
-            <li class="lista-fotos-item" v-for="user in users" :key="user.name">
-                <p>{{user.name}}</p>
-                
-
-            </li>
-        </ul>
+        <el-row v-for="user in users" :key="user.name">
+            <hr>
+            <el-col :span="4"><div class=""></div>{{user.registerDate}}</el-col>
+            <el-col :span="4"><div class=""></div>{{user.name}}</el-col>
+            <el-col :span="4"><div class=""></div>{{user.age}}</el-col>
+            <el-col :span="4"><div class=""></div>{{user.email}}</el-col>
+            <el-col :span="4"><div class=""></div>{{user.login}}</el-col>
+            
+        </el-row>
 
         <hr><br>
-        
-        <div v-for=" user in users" :key="user.name"> {{user.name}}</div>
         <el-button type="primary" icon="el-icon-edit" circle @click="loadUsers()"></el-button>
 
 
@@ -33,7 +53,24 @@ export default {
     
     data() {
         return {
-            users: []
+            users: [],
+            tableData: [{
+            date: '2016-05-03',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-02',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-04',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }, {
+            date: '2016-05-01',
+            name: 'Tom',
+            address: 'No. 189, Grove St, Los Angeles'
+          }]
         }
     },
     computed: {
