2d2e3ef2e977033211396a79a3eee992e6ca83fe
ThyagoMac <thyago.cormick@gmail.com>
2020-07-03 08:04:57 -0300

    Gitpod try

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+//import 'element-ui/lib/theme-chalk/index.css';
+import './css/main.css';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
